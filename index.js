68d7b2ebfcd81357f514133a63dfdc43331d5937
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-17 14:37:53 +0500

    work

---
+++ index.js
@@ -0,0 +1,6 @@
+//  var monNames = ["January"," February","March","April"," May","June","July","August","September","October","November","December"];
+//  var now = new Date();
+//  var themon = now.getMonth();
+//  var nameOfmon = monNames[themon];
+//  console.log(nameOfmon);
+
