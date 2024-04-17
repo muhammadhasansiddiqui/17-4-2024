07f52c12a19a5e9da517958bbb32286772f4a040
muhammadhasansiddiqui <hasansiddiqui464@gmail.com>
2024-04-17 15:21:28 +0500

    work

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
-//  var monNames = ["January"," February","March","April"," May","June","July","August","September","October","November","December"];
-//  var now = new Date();
-//  var themon = now.getMonth();
+//  var monNames  = ["January"," February","March","April"," May","June","July","August","September","October","November","December"];
+//  var now       = new Date();
+//  var themon    = now.getMonth();
 //  var nameOfmon = monNames[themon];
-//  console.log(nameOfmon);
+//  console.log   (nameOfmon);
+
+
+// var monNames   = ["January", " February", "March", "April", " May", "June", "July", "August", "September", "October", "November", "December"];
+// var dateNames  = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",]
+// var dayNames   = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var now        = new Date();
+// var themon     = now.getMonth();
+// var thedate    = now.getDate();
+// var theday     = now.getDay();
+// var nameOfmon  = monNames[themon];
+// var nameOfdate = dateNames[thedate];
+// console.log    (nameOfmon);
+// console.log    (nameOfdate);
+// console.log    (theday);
+
+
+// var doomsday    = new Date("June 30, 2035");
+// console.log     (doomsday.getTime());
+// console.log     (doomsday.getFullYear());
+// console.log     (doomsday.getMonth());
+// console.log     (doomsday.getDay());
+
+// var now = new Date();
+// var time = now.getTime();
+// console.log("Now ===>",now);
+// console.log("Time ===>",time );
+
 
